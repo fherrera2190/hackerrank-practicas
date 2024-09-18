d595d7e8f67683aef194a6f78a185a662fedf605
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-09-18 19:37:51 -0300

    add formingmagicsquare done!

---
+++ week6/FormingMagicSquare.js
@@ -0,0 +1,15 @@
+function formingMagicSquare(s) {
+    // Write your code here
+    const patterns = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+    ];
+    const getCost = (pattern, s) => pattern.reduce((cost, value, index) => cost + Math.abs(value - s[index]), 0);
+    return patterns.reduce((cost, pattern) => Math.min(cost, getCost(pattern.flat(), s.flat())), Infinity);
+}
---
+++ week6/PrimeDates.js
@@ -0,0 +1,46 @@
+function processData(input) {
+  //Enter your code here
+  const [startDate, endDate] = input.trim().split(" ");
+  let [day1, month1, year1] = startDate.split("-").map(Number);
+  let [day2, month2, year2] = endDate.split("-").map(Number);
+
+  const maxDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let count = 0;
+
+  while (true) {
+    const dateValue = parseInt(`${day1}${month1}${year1}`, 10);
+
+    if (dateValue % 4 === 0 || dateValue % 7 === 0) {
+      count++;
+    }
+
+    if (day1 === day2 && month1 === month2 && year1 === year2) {
+      break;
+    }
+
+    day1++;
+
+    if (findPrimeDates(year1)) {
+      maxDays[1] = 29;
+    } else {
+      maxDays[1] = 28;
+    }
+
+    if (day1 > maxDays[month1 - 1]) {
+      day1 = 1;
+      month1++;
+    }
+
+    if (month1 > 12) {
+      month1 = 1;
+      year1++;
+    }
+  }
+
+  console.log(count);
+}
+function findPrimeDates(year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+
+processData("02-08-2025 04-09-2025");
